23ec2d777131f52aee3dbcb86a98c065d07bab8b
mvasylykiv <mvasylykiv@griddynamics.com>
2019-12-10 11:42:00 +0200

    moved class to viewControls, removed redundant props

--- components/ViewControls.vue
+++ components/ViewControls.vue
@@ -32,14 +32,52 @@
   </div>
 </template>
 
+<style lang="scss">
+  @import "~/assets/css/vars";
+
+  .view-controls {
+    margin-left: 20px;
+
+    div {
+      height: 24px;
+      border: 1px solid #dadada;
+      background-color: #fff;
+      cursor: pointer;
+      position: relative;
+
+      &:hover {
+        span {
+          background-color: #999;
+        }
+      }
+
+      &.active {
+        cursor: default;
+        background-color: #fff;
+        span {
+          background-color: #000;
+        }
+      }
+
+      span {
+        display: block;
+        position: absolute;
+        width: 5px;
+        height: 4px;
+        background-color: #b7b7b7;
+        transition: background-color ease 0.1s;
+      }
+    }
+  }
+</style>
+
 <script>
   export default {
     props: [
       "activateGridView",
       "activateListView",
       "isGridView",
-      "isListView",
-      "active"
+      "isListView"
     ]
   }
 </script>
