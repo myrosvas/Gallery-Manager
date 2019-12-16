13168b009f7b33bb4d352661246ed714dcd8fc17
mvasylykiv <mvasylykiv@griddynamics.com>
2019-12-16 12:47:47 +0200

    changed scss for images

--- components/QuickView.vue
+++ components/QuickView.vue
@@ -68,7 +68,6 @@
 
   img {
     margin-bottom: 10px;
-    max-height: calc(100vh - 180px);
     opacity: 0;
     transition: opacity 0.15s;
   }
@@ -76,11 +75,10 @@
   .v-lazy-image {
     max-height: 0;
     transition: max-height 0.15s ease-out;
-    transition: filter 0.7s;
   }
   
   .v-lazy-image-loaded {
-    max-height: 500px;
+    max-height: calc(100vh - 180px);
     transition: max-height 0.25s ease-in;
     opacity: 1;
   }
